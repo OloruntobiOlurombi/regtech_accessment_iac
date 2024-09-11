d54d21cecb4760f5d739493c6abc08d2c2e75861
Oloruntobi Olurombi <oloruntobiolurombi@gmail.com>
2024-09-11 17:29:41 +0100

    add issuer@v4

--- oidc.tf
+++ oidc.tf
@@ -1,5 +1,5 @@
 data "tls_certificate" "eks" {
-  url = aws_eks_cluster.eks_cluster.identity[0].oidc.issuer
+  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
 
 resource "aws_iam_openid_connect_provider" "eks" {
