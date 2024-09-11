e37e6ce5ec65931df5deb1a2ba4438ec3da1b506
Oloruntobi Olurombi <oloruntobiolurombi@gmail.com>
2024-09-11 11:09:05 +0100

    update version eks and config-update

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
 
-  #version = "1.28.11"
+  version = "1.28"
 
   vpc_config {
     subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id]
