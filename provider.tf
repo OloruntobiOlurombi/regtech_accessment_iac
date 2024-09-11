60f15c92a5bd94e556ecf60c7eb55311be99c175
Oloruntobi Olurombi <oloruntobiolurombi@gmail.com>
2024-09-11 06:36:11 +0100

    test:v2

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = var.region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key 
-  #profile = "tobi"
+#   profile = "tobi"
 }
 
 #profile = var.profile
